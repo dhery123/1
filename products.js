8eac9bbcdd3d8cee498c5b5e29bcdf5ff3337bef
Dhery <reptex123@gmail.com>
2021-02-08 07:43:34 -0400

    Firs version

---
+++ products.js
@@ -0,0 +1,16 @@
+class product{
+    constructor(id, name, descripcion, quatity){
+        this.id = id;
+        this.name = name;
+        this.descripcion = descripcion;
+        this.quatity = quatity;
+    }
+}
+class ProductManagement{
+    constructor(){
+        this.listproducts = new Array();
+    }
+
+
+
+}
