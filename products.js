4a14acd72e5cfcb957bf5a588af5033a85e33336
Dhery <reptex123@gmail.com>
2021-02-08 12:21:39 -0400

    products

--- products.js
+++ products.js
@@ -1,20 +1,36 @@
-class product{
+class Product{
     constructor(id, name, descripcion, quatity){
         this.id = id;
         this.name = name;
         this.descripcion = descripcion;
         this.quatity = quatity;
     }
+    getId(){
+        return this.id;
+    }
+    getName(){
+        return this.name;
+    }
+    getDescripcion(){
+        return this.descripcion;
+    }
+    getQuantity(){
+        return this.quatity;
+    }
 }
 class ProductManagement{
-    constructor(){
+    constructor() {
         this.listproducts = new Array();
     }
-    addproduct(product){
-        this.listproducts.push(product)
+    addProduct(name, descripcion, quatity) {
+        var date = new Date();
+        var product = new Product(date, name, descripcion, quatity);
+        this.listproducts.push(product);
     }
-    removeProduct(product){
-        for(var i=0; i< this.listproducts.length; i++){
+    
+    removeProduct(product)
+    {
+        for (var i=0; i< this.listproducts.length; i++){
             if(this.listproducts[i] == product){
                 this.listproducts.splice(i, 1);
                 return true;
@@ -23,5 +39,17 @@ class ProductManagement{
         }
         return false;
     }
+    updateProducts(product, updateproduct){
+        for(var i=0; i < this.listproducts.length; i++){
+            if (this.listproducts[i] == product){
+                this.listproducts[i] = updateproduct;
+                return true
+            }
+        }
+        return false
+    }
+    getList(){
+        return this.listproducts;
+    }
 }
-export{product, ProductManagement}
+export {ProductManagement};
---
+++ test.js
@@ -0,0 +1,5 @@
+import { ProductManagement } from "./products.js";
+var p = new ProductManagement();
+p.addProduct("Pollo", "Pollo sofia", 12);
+var lista = p.getList();
+console.log(lista);
